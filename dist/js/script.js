29d36f80244a4256df884fb1291504e511bf0346
imFranMorais <marcus_deslandes@hotmail.com>
2026-09-14 14:51:23 -0300

    adicionado funcionalidade das dicas flutuantes

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,14 +7,23 @@ const previewImagem = document.querySelector("#image-preview");
 
 const botaoCamera = document.querySelector("#btn-camera");
 
+const caixaDica = document.querySelector('#dica-container');
+
+const paragrafoDica = document.querySelector('#dica-paragrafo');
+
 botaoFoto.addEventListener('click', () => {
     // Interação entre botões de tipo de câmera e botão da câmera
     botaoCamera.classList.replace('bg-red-700', 'bg-white');
     previewImagem.src = "./images/photo-ex1.jpg";
     botaoVideo.classList.remove("bg-[#120052]",  "rounded-full");
     botaoDocumento.classList.remove("bg-[#120052]",  "rounded-full");
-    botaoFoto.classList.add("bg-[#120052]",  "rounded-full")
-})
+    botaoFoto.classList.add("bg-[#120052]",  "rounded-full");
+
+    // dica
+    paragrafoDica.textContent = 'Que tal dar um passo para a direita?'
+    caixaDica.classList.add('flex');
+    caixaDica.classList.remove('hidden');
+});
 
 botaoVideo.addEventListener('click', () => {
     // Interação entre botões de tipo de câmera e botão da câmera
@@ -22,8 +31,14 @@ botaoVideo.addEventListener('click', () => {
     previewImagem.src = "./images/photo-ex2.jpg";
     botaoFoto.classList.remove("bg-[#120052]",  "rounded-full");
     botaoDocumento.classList.remove("bg-[#120052]",  "rounded-full");
-    botaoVideo.classList.add("bg-[#120052]",  "rounded-full")
-})
+    botaoVideo.classList.add("bg-[#120052]",  "rounded-full");
+
+     // dica
+    paragrafoDica.textContent = 'Estabilize um pouco a câmera!'
+    caixaDica.classList.add('flex');
+    caixaDica.classList.remove('hidden');
+
+});
 
 botaoDocumento.addEventListener('click', () => {
     // Interação entre botões de tipo de câmera e botão da câmera
@@ -31,5 +46,10 @@ botaoDocumento.addEventListener('click', () => {
     previewImagem.src = "./images/photo-ex3.jpg";
     botaoFoto.classList.remove( "bg-[#120052]",  "rounded-full");
     botaoVideo.classList.remove("bg-[#120052]",  "rounded-full");
-    botaoDocumento.classList.add("bg-[#120052]",  "rounded-full")
-})
+    botaoDocumento.classList.add("bg-[#120052]",  "rounded-full");
+
+    // dica
+    paragrafoDica.textContent = 'Afaste a câmera para enquadrar todo o documento!'
+    caixaDica.classList.add('flex');
+    caixaDica.classList.remove('hidden');
+});
