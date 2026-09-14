0a09c4811268f3ed59bc1f8ae755fb371a010a06
imFranMorais <marcus_deslandes@hotmail.com>
2026-09-14 12:10:23 -0300

    fix: correção dos botões de tipo de camera

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,23 +11,23 @@ botaoFoto.addEventListener('click', () => {
     // Interação entre botões de tipo de câmera e botão da câmera
     botaoCamera.classList.replace('bg-red-700', 'bg-white');
     previewImagem.src = "./images/photo-ex1.jpg";
-    botaoVideo.classList.remove("px-4", "py-1" , "bg-[#120052]",  "rounded-full");
-    botaoDocumento.classList.remove("px-4", "py-1" , "bg-[#120052]",  "rounded-full");
-    botaoFoto.classList.add("px-4", "py-1" , "bg-[#120052]",  "rounded-full")
+    botaoVideo.classList.remove("bg-[#120052]",  "rounded-full");
+    botaoDocumento.classList.remove("bg-[#120052]",  "rounded-full");
+    botaoFoto.classList.add("bg-[#120052]",  "rounded-full")
 })
 botaoVideo.addEventListener('click', () => {
     // Interação entre botões de tipo de câmera e botão da câmera
     botaoCamera.classList.replace('bg-white', 'bg-red-700');
     previewImagem.src = "./images/photo-ex2.jpg";
-    botaoFoto.classList.remove("px-4", "py-1" , "bg-[#120052]",  "rounded-full");
-    botaoDocumento.classList.remove("px-4", "py-1" , "bg-[#120052]",  "rounded-full");
-    botaoVideo.classList.add("px-4", "py-1" , "bg-[#120052]",  "rounded-full")
+    botaoFoto.classList.remove("bg-[#120052]",  "rounded-full");
+    botaoDocumento.classList.remove("bg-[#120052]",  "rounded-full");
+    botaoVideo.classList.add("bg-[#120052]",  "rounded-full")
 })
 botaoDocumento.addEventListener('click', () => {
     // Interação entre botões de tipo de câmera e botão da câmera
     botaoCamera.classList.replace('bg-red-700', 'bg-white');
     previewImagem.src = "./images/photo-ex3.jpg";
-    botaoFoto.classList.remove("px-4", "py-1" , "bg-[#120052]",  "rounded-full");
-    botaoVideo.classList.remove("px-4", "py-1" , "bg-[#120052]",  "rounded-full");
-    botaoDocumento.classList.add("px-4", "py-1" , "bg-[#120052]",  "rounded-full")
+    botaoFoto.classList.remove( "bg-[#120052]",  "rounded-full");
+    botaoVideo.classList.remove("bg-[#120052]",  "rounded-full");
+    botaoDocumento.classList.add("bg-[#120052]",  "rounded-full")
 })
